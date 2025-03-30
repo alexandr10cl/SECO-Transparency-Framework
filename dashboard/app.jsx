36f7cf5c06984689b255e2717e55db2f39709433
jouw naam <jouw-email@voorbeeld.com>
2025-03-30 22:31:58 +0200

    implemented secodashboard

---
+++ dashboard/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SECODashboard from './dashboard';
+import './index.css'; // For Tailwind CSS
+
+function App() {
+  return (
+    <div className="container mx-auto py-4 px-2">
+      <SECODashboard />
+    </div>
+  );
+}
+
+export default App;
